db4c64562abfda74d02b7dc14ee1b5339d5756c0
kelvin926 <kelvin@kakao.com>
2023-02-05 04:54:57 +0900

    라인트레이싱 중앙 Fin

--- src/speed_racing/src/control.py
+++ src/speed_racing/src/control.py
@@ -8,15 +8,74 @@ from geometry_msgs.msg import Twist
 class move_limo:
     def __init__(self):
         rospy.init_node('control', anonymous=True)
-        self.ref_x = 320
+        self.ref_x = 160
         self.BASE_SPEED = 0.2
-        self.LATERAL_GAIN = 0.1 # 가중치
+        self.LATERAL_GAIN = 0.005
 
 
-        # self.traffic_light_last_time = rospy.Time.now().to_sec()
+        self.traffic_light_last_time = rospy.Time.now().to_sec()
 
-        self.left_x = rospy.Subscriber("/lane/left_lane_x", Int32, self.lane_cb)
-        self.right_x = rospy.Subscriber("/lane/right_lane_x", Int32, self.lane_cb)
+        self.left_lane_x = rospy.Subscriber("/lane/left_lane_x", Int32, self.left_lane_cb)
+        self.right_lane_x = rospy.Subscriber("/lane/right_lane_x", Int32, self.right_lane_cb)
+
+        self.drive_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+
+        rospy.Timer(rospy.Duration(0.03), self.drive_control)
+
+
+    def drive_control(self, event):
+# 센터의 x좌표 값은 320이다.
+        try:
+            self.off_center = (self.left_x + self.right_x) / 2 - self.ref_x
+            rospy.loginfo("off_center, lateral_gain = {}, {}".format(self.off_center, self.LATERAL_GAIN))
+            drive = Twist()
+            drive.linear.x = self.BASE_SPEED
+            drive.angular.z = self.off_center * self.LATERAL_GAIN
+            self.drive_pub.publish(drive)
+
+        except:
+            pass 
+
+
+    def left_lane_cb(self, data):
+        if data.data == -1:
+            self.left_x = 0
+        else:
+            self.left_x = data.data 
+            
+    def right_lane_cb(self, data):
+        if data.data == -1:
+            self.right_x = 0
+        else:
+            self.right_x = data.data 
+            
+
+
+if __name__ == '__main__':
+    MoveCar = move_limo()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("program down")
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import rospy
+from std_msgs.msg import Int32, String
+import math
+from geometry_msgs.msg import Twist
+
+class move_limo:
+    def __init__(self):
+        rospy.init_node('control', anonymous=True)
+        self.ref_x = 160
+        self.BASE_SPEED = 0.2
+        self.LATERAL_GAIN = 0.005
+
+
+        self.traffic_light_last_time = rospy.Time.now().to_sec()
+
+        self.left_lane_x = rospy.Subscriber("/lane/left_lane_x", Int32, self.left_lane_cb)
+        self.right_lane_x = rospy.Subscriber("/lane/right_lane_x", Int32, self.right_lane_cb)
 
         self.drive_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
 
@@ -25,24 +84,29 @@ class move_limo:
 
     def drive_control(self, event):
 
-# 센터의 x좌표 값은 320이다.
-            try:
-                self.off_center = (self.left_x + self.right_x) / 2 - self.ref_x
-                rospy.loginfo("off_center, lateral_gain = {}, {}".format(self.off_center, self.LATERAL_GAIN))
-                drive = Twist()
-                drive.linear.x = self.BASE_SPEED
-                drive.angular.z = self.off_center * self.LATERAL_GAIN
-                self.drive_pub.publish(drive)
-                
-            except:
-                pass 
-                
+        try:
+            self.off_center = (self.left_x + self.right_x) / 2 - self.ref_x
+            rospy.loginfo("off_center, lateral_gain = {}, {}".format(self.off_center, self.LATERAL_GAIN))
+            drive = Twist()
+            drive.linear.x = self.BASE_SPEED
+            drive.angular.z = self.off_center * self.LATERAL_GAIN
+            self.drive_pub.publish(drive)
 
-    def lane_cb(self, data):
+        except:
+            pass 
+
+
+    def left_lane_cb(self, data):
         if data.data == -1:
-            return 0
+            self.left_x = 0
         else:
-            return data.data 
+            self.left_x = data.data 
+            
+    def right_lane_cb(self, data):
+        if data.data == -1:
+            self.right_x = 0
+        else:
+            self.right_x = data.data 
             
 
 
--- src/speed_racing/src/lane_detect.py
+++ src/speed_racing/src/lane_detect.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+# -*- coding:utf-8 -*-
 import cv2
 import numpy as np
 from sensor_msgs.msg import Image
@@ -17,11 +17,11 @@ class LaneDetection:
         self.steering_right_pub = rospy.Publisher("/lane/right_lane_x", Int32, queue_size=1) # 차선의 오른쪽 x좌표를 publish
 
 
-        self.YELLOW_LANE_LOW = np.array([5,90,179]) # BGR로 처리 (노란색 차선)
+        self.YELLOW_LANE_LOW = np.array([5,90,179]) # HLS로 처리 (노란색 차선)
         self.YELLOW_LANE_HIGH = np.array([35,255,255])
 
-        self.WHITE_LANE_LOW = np.array([200,200,200]) # BGR로 처리 (흰색 차선)
-        self.WHITE_LANE_HIGH = np.array([255,255,255])
+        self.WHITE_LANE_LOW = np.array([0,200,0]) # HLS로 처리 (흰색 차선)
+        self.WHITE_LANE_HIGH = np.array([179,255,255])
 
 
     def Image_process_callback(self, img):
@@ -57,10 +57,10 @@ class LaneDetection:
     def viz_result(self):
         
         cv2.imshow('original image',self.frame)
-        cv2.imshow('left cropped image',self.left_cropped_image)
-        cv2.imshow('right cropped image',self.right_cropped_image)
-        cv2.imshow('left color_detect',self.left_thresholded_image)
-        cv2.imshow('right color_detect',self.right_thresholded_image)
+        #cv2.imshow('left cropped image',self.left_cropped_image)
+        #cv2.imshow('right cropped image',self.right_cropped_image)
+        #cv2.imshow('left color_detect',self.left_thresholded_image)
+        #cv2.imshow('right color_detect',self.right_thresholded_image)
         self.mask_yellow = cv2.circle(self.left_thresholded_image,(self.left_x,30),10,(0,0,0),-1)
         self.mask_white = cv2.circle(self.right_thresholded_image,(self.right_x,30),10,(0,0,0),-1)
         cv2.imshow('yellow_color_detect_circle',self.mask_yellow)
